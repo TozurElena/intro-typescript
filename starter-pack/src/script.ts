9b4323f14040db001897e69de14a30e32d6d769d
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-06-30 11:16:47 +0200

    modifiet js to ts intro starter-pack

--- starter-pack/src/script.ts
+++ starter-pack/src/script.ts
@@ -1,21 +1,26 @@
-(function () {
+(function() {
     document.getElementById('guess-form').addEventListener("submit", compareUserGuess);
-    function compareUserGuess(event) {
+
+    function compareUserGuess (event) {
         event.preventDefault();
-        var secretNumber = generateSecretNumber();
-        var userNumber = document.getElementById('guess').value;
+
+        const secretNumber: number = generateSecretNumber();
+        const userNumber: number = document.getElementById('guess').value;
+
         if (secretNumber == userNumber) {
             alert("Awesome! You number " + userNumber + " was correct. You can be named many things, hungry not being one of them.");
-        }
-        else {
+        } else {
             alert("Bummer... You guessed " + userNumber + " and the secret number was " + secretNumber + ".");
         }
     }
-    function generateSecretNumber() {
-        var secretNumber = getRandomArbitrary(1, 10);
+
+    function generateSecretNumber(): number {
+        const secretNumber: number = getRandomArbitrary(1, 10);
+
         return secretNumber;
     }
-    function getRandomArbitrary(min, max) {
+
+    function getRandomArbitrary(min: number, max: number): number {
         return Math.round(Math.random() * (max - min) + min);
     }
 })();
